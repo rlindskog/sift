d3b08d830b9b2529199ae698997d1af8d603fe0d
Ryan Lindskog <r.lindskog17@gmail.com>
2017-05-06 17:43:10 -0700

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,17 @@
 require('dotenv').config()
 
 module.exports = {
+	head: {
+		link: [
+			{ href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons', rel: 'stylesheet'},
+			// <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet" type="text/css">
+			{ href: 'https://unpkg.com/vuetify/dist/vuetify.min.css', rel: 'stylesheet'},
+			// <link href="https://unpkg.com/vuetify/dist/vuetify.min.css" rel="stylesheet" type="text/css">
+		],
+		script: [
+			{ src: 'https://unpkg.com/vuetify/dist/vuetify.min.js'}
+		]
+	},
 	css: [
   	'assets/main.css',
   	'assets/normalize.css'
@@ -16,5 +27,9 @@ module.exports = {
 		API_HOST: '127.0.0.1',
 		API_PORT: 3000,
 		API_URL: 'http://127.0.0.1:3000/api'
+  },
+  vue: {
+  	middleware: 'vuetify'
   }
 }
+
