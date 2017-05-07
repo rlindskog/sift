d57c7a35257daed07c96da0aa4ef98e6e617ac07
Ryan Lindskog <r.lindskog17@gmail.com>
2017-05-06 22:30:20 -0700

    Maintaining user state through page requests

--- server/api/users/routes.js
+++ server/api/users/routes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import usersModels from './models'
 import { users, username, signIn } from './controllers'
-
+import authenticate from '../../middleware/authenticate'
 const router = express.Router()
 
 router.get('/', users.get)
@@ -10,9 +10,9 @@ router.post('/', users.post)
 router.post('/signin', signIn.post)
 // router.get('/signin', signIn.post)
 
-router.get('/:user', username.get)
-router.post('/:user', username.post)
-router.put('/:user', username.put)
-router.delete('/:user', username.delete)
+router.get('/:user', authenticate, username.get)
+router.post('/:user', authenticate, username.post)
+router.put('/:user', authenticate, username.put)
+router.delete('/:user', authenticate, username.delete)
 
 export default router
